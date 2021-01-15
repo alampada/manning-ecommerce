32b8b5f152cacfd88ea2950162e106b328c13b07
Antonios Lampadaridis <antonios.lampadaridis@gmail.com>
2021-01-15 16:31:20 +0000

    1.1 Add homepage with recommended template

--- src/main/java/com/ala/manningecommerce/controller/CakeController.java
+++ src/main/java/com/ala/manningecommerce/controller/CakeController.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class CakeController {
 
 	@GetMapping
-	public String helloWorld() {
-		return "hello world";
+	public String homepage() {
+		return "index";
 	}
 }
