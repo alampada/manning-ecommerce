567c2da2d5c592ba32f4b4082b16d258e96e3717
Antonios Lampadaridis <antonios.lampadaridis@gmail.com>
2021-01-15 16:12:44 +0000

    Initial Commit

---
+++ src/main/java/com/ala/manningecommerce/ManningEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ala.manningecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManningEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManningEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ala/manningecommerce/ManningEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ala.manningecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManningEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
