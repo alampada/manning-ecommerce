cf5eedc2e5a11e4b05d5dfc9be7b38793a81b87b
Antonios Lampadaridis <antonios.lampadaridis@gmail.com>
2021-01-15 16:41:07 +0000

    1.1 Modify template and add controller Integration test

--- src/main/java/com/ala/manningecommerce/controller/CakeController.java
+++ src/main/java/com/ala/manningecommerce/controller/CakeController.java
@@ -3,9 +3,6 @@ package com.ala.manningecommerce.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
 
 @Controller
 @RequestMapping("/")
